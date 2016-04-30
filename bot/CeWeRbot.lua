e293b88072d6f45ed7ba371d0ed7c8f95271b9c1
amirf2020 <arminghafouri8869.8869@gmail.com>
2016-04-30 12:50:56 -0700

    Update CeWeRbot.lua

--- bot/CeWeRbot.lua
+++ bot/CeWeRbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {134618808,212397342,tonumber(our_id)},--Sudo users
+    sudo_users = {196560568,212397342,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[CeWeR v2 - Open Source
